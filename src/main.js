3dd466303f0e970d8aa82f897beb11c188a1a0a1
李浩宇 <59479319+JamesLHY@users.noreply.github.com>
2021-01-29 14:44:24 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 let html = document.querySelector("#html");
 let style = document.querySelector("#style");
 let string = `
-/* 你好，我叫L
- *接下来我演示一下我的前端功底
- *首先准备一个div
+/* 首先准备一个div
  */
 #div1{
   border: 1px solid red;
   width: 200px;
   height: 200px;
 }
-/*接下来我把div变成一个八卦图
- *注意看好了
+/*接下来把div变成一个八卦图
  *首先，把div变成一个圆
  */
 #div1{
@@ -19,17 +16,15 @@ let string = `
     box-shadow: 0 0 3px rgba(0,0,0,0.5);
     border: none;
 }
-/*八卦是阴阳形成的
- *一黑一白
+/*八卦图一黑一白
  */
 #div1{
     background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%, rgba(255,255,255,1) 50%, rgba(255,255,255,1) 100%);
 }
-/*加两个球*/
+/*最后加两个球*/
 #div1::before{
     width:100px;
     height:100px;
-    
     bottom:0;
     left:50%;
     transform:translateX(-50%);
@@ -40,7 +35,6 @@ let string = `
 #div1::after{
     width:100px;
     height:100px;
-
     top:0;
     left:50%;
     transform:translateX(-50%);
